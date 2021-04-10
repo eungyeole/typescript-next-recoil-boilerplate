10e2a57a24a1cd84b8f674d14919d5eb6b748a7b
eungyeole <eungyeole@naver.com>
2021-04-10 17:38:49 +0900

    [UPDATE] <pages> todoList

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,14 @@
+import { TodoItem, TodoList } from "@/src/components/todo";
+import useSWR from 'swr';
+import todoList from "@/src/libs/api/todoList";
+
+const fetcher = async () => (await todoList.getTodoList()).data;
+
 export default function Home() {
+  const { data, error} = useSWR('/api/todo', fetcher);
+  if(error) alert(error);
+  if (!data) return <div>loading...</div>;
   return (
-    <div></div>
+    <TodoList todo={data} />    
   )
 }
