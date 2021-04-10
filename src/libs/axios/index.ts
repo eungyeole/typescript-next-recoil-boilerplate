a8bab513a200249be3010e7a9cd846ad39249cd9
eungyeole <eungyeole@naver.com>
2021-04-10 14:29:43 +0900

    [INIT] create axios instatnce

---
+++ src/libs/axios/index.ts
@@ -0,0 +1,23 @@
+import axios, { AxiosError, AxiosRequestConfig, AxiosResponse } from 'axios'
+
+const instance = axios.create({
+    baseURL: process.env.NEXT_PUBLIC_URL,
+});
+instance.interceptors.request.use(
+    function (config : AxiosRequestConfig) {
+        return config;
+    }, 
+    function (error : AxiosError) {
+        return Promise.reject(error);
+    }
+);
+instance.interceptors.response.use(
+    function (response : AxiosResponse) {
+        return response;
+    },
+
+    function (error : AxiosError) {
+        return Promise.reject(error);
+    }
+);
+export default instance;
