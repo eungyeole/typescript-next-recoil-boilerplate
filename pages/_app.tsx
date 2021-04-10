7d71737c00500472b3f539071a8caa65b7146dc5
eungyeole <eungyeole@naver.com>
2021-04-10 14:21:42 +0900

    [INIT] global style

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,15 @@
+import { globalStyle } from '@/src/styles/globalStyle'
+import theme from '@/src/styles/theme'
+import { Global, ThemeProvider } from '@emotion/react'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider theme={theme}>
+      <Global styles={globalStyle}/>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  )
 }
 
 export default MyApp
---
+++ src/styles/globalStyle.ts
@@ -0,0 +1,12 @@
+import { css } from "@emotion/react";
+
+export const globalStyle = css`
+  * {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+    list-style: none;
+    outline: none;
+    text-decoration : none;
+  }
+`;
